713572c3ae8000251a50642937517ca114db3484
Guslarz <mrkaczmareks@gmail.com>
2022-01-18 21:17:51 +0100

    Add tabs

--- Ui/Converters/VisibilityConverter.cs
+++ Ui/Converters/VisibilityConverter.cs
@@ -1,15 +1,17 @@
 ﻿using System;
 using System.Globalization;
+using System.Windows;
 using System.Windows.Data;
 
 namespace Kaczmarek.BeersCatalogue.Ui.Converters
 {
-    [ValueConversion(typeof(bool), typeof(bool))]
-    public class NullableBooleanConverter : IValueConverter
+
+    [ValueConversion(typeof(object), typeof(Visibility))]
+    public class VisibilityConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return (bool)(value ?? false);
+            return value == null ? Visibility.Collapsed : Visibility.Visible;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
